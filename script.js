26c4add9b4ada7499ebf42e34b35abd1fb3b9495
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-11 19:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function validEmail(str) {
   //your JS code here.
+	email_pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    
+    // # Using re.match() to check if the provided email matches the pattern
+    if re.match(email_pattern, str){
+        return True}
+    else{
+        return False}
 }
 
 // Do not change the code below.
